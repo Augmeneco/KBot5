e2dc256afe37b495a15ff44bfb10d53ae3d0384e
Cha14ka <taitor@yandex.ru>
2019-06-29 14:58:40 +0300

    KBOT5 HAS ARRIVED!

--- main.py
+++ main.py
@@ -30,74 +30,79 @@ for path in os.listdir('plugins'):
 
 start_time = time.monotonic()	
 while True:
-	active = False
 	try:
-		response = requests.post(lpb['server']+'?act=a_check&key='+lpb['key']+'&ts='+str(ts)+'&wait=25').json()
-		ts = response['ts']
-	except Exception as error:
-		if error == KeyboardInterrupt:
-			sys.exit(0)
-		lpb = requests.post('https://api.vk.com/method/groups.getLongPollServer',data={'access_token':config['group_token'],'v':'5.80','group_id':config['group_id']}).json()['response']
-		ts = lpb['ts']
-		continue
+		active = False
+		try:
+			response = requests.post(lpb['server']+'?act=a_check&key='+lpb['key']+'&ts='+str(ts)+'&wait=25').json()
+			ts = response['ts']
+		except:
+			lpb = requests.post('https://api.vk.com/method/groups.getLongPollServer',data={'access_token':config['group_token'],'v':'5.80','group_id':config['group_id']}).json()['response']
+			ts = lpb['ts']
+			continue
 
-	for result in response['updates']:
-		text = result['object']['text']
-		payload = None
-		msgid = result['object']['conversation_message_id']
-		if 'payload' in result['object']:
-			payload = result['object']['payload']
-		if '@kbot5' in text:
-			text = re.sub('\[club\d*\|@kbot5\]','@kbot5',text)
-		text_split = text.split(' ')
-		if text_split[0].lower() in config['names']:
-			active = True
-		if payload == '{"command":"start"}':
-			active = True
-			text = '@kbot5 помощь'
+		for result in response['updates']:
+			text = result['object']['text']
+			payload = None
+			msgid = result['object']['conversation_message_id']
+			if 'payload' in result['object']:
+				payload = result['object']['payload']
+			if '@kbot5' in text:
+				text = re.sub('\[club\d*\|@kbot5\]','@kbot5',text)
 			text_split = text.split(' ')
-		if len(text_split) > 2: user_text = text.replace(re.findall('\S* \S* ',text)[0],'')
-		else: user_text = ''
-		if text == 'F' and result['object']['from_id'] > 0: apisay('F',result['object']['peer_id'])
-		
-		if active:
-			toho = result['object']['peer_id']
-			userid = result['object']['from_id']
-			pack = {}
-			pack['text'] = text
-			pack['toho'] = toho
-			pack['userid'] = userid
-			pack['payload'] = payload
-			pack['msgid'] = msgid
-			pack['user_text'] = user_text
-			pack['text_split'] = text_split
-			lastmsgid = msgid
-			users_db_tmp = users_db.cursor().execute('SELECT * FROM users WHERE id = '+str(userid)).fetchall()
+			if text_split[0].lower() in config['names']:
+				active = True
+			if payload == '{"command":"start"}':
+				active = True
+				text = '@kbot5 помощь'
+				text_split = text.split(' ')
+			if len(text_split) > 2: user_text = text.replace(re.findall('\S* \S* ',text)[0],'')
+			else: user_text = ''
+			if text == 'F' and result['object']['from_id'] > 0: apisay('F',result['object']['peer_id'])
 			
-			if len(users_db_tmp) != 0:
-				user_mode = users_db_tmp[0][1]
-			else: user_mode = 1
+			if active:
+				toho = result['object']['peer_id']
+				userid = result['object']['from_id']
+				pack = {}
+				pack['text'] = text
+				pack['toho'] = toho
+				pack['userid'] = userid
+				pack['payload'] = payload
+				pack['msgid'] = msgid
+				pack['user_text'] = user_text
+				pack['text_split'] = text_split
+				lastmsgid = msgid
+				users_db_tmp = users_db.cursor().execute('SELECT * FROM users WHERE id = '+str(userid)).fetchall()
 				
-			if text_split[1] in cmds:
-				if cmds[text_split[1]][0] == 'admin' and user_mode == 3:
-					threading.Thread(target=do_cmd,args=(commands['admin'][cmds[text_split[1]][1]],pack)).start()
-					continue
-				elif cmds[text_split[1]][0] == 'admin' and user_mode != 3:
-					apisay('Пшол вон из админки',toho)
-					continue
-				if cmds[text_split[1]][0] == 'vip' and (user_mode == 3 or user_mode == 2):
-					threading.Thread(target=do_cmd,args=(commands['vip'][cmds[text_split[1]][1]],pack)).start()
-					continue
-				elif cmds[text_split[1]][0] == 'admin' and (user_mode != 3 or user_mode != 2):
-					apisay('У вас нет доступа к вип командам',toho)
-					continue
-				if user_mode == 0:
-					apisay('Вам бан',toho)
-					continue
-				if user_mode > 0:
-					threading.Thread(target=do_cmd,args=(commands['default'][cmds[text_split[1]][1]],pack)).start()		
-					continue			
-			else:
-				speak = requests.post('https://isinkin-bot-api.herokuapp.com/1/talk',data={'q':user_text}).json()
-				if 'text' in speak: apisay(speak['text'],toho)
-				else: apisay('Команда не найдена :(', toho)
+				if len(users_db_tmp) != 0:
+					user_mode = users_db_tmp[0][1]
+				else: user_mode = 1
+					
+				if text_split[1] in cmds:
+					if cmds[text_split[1]][0] == 'admin' and user_mode == 3:
+						threading.Thread(target=do_cmd,args=(commands['admin'][cmds[text_split[1]][1]],pack)).start()
+						continue
+					elif cmds[text_split[1]][0] == 'admin' and user_mode != 3:
+						apisay('Пшол вон из админки',toho)
+						continue
+					if cmds[text_split[1]][0] == 'vip' and (user_mode == 3 or user_mode == 2):
+						threading.Thread(target=do_cmd,args=(commands['vip'][cmds[text_split[1]][1]],pack)).start()
+						continue
+					elif cmds[text_split[1]][0] == 'admin' and (user_mode != 3 or user_mode != 2):
+						apisay('У вас нет доступа к вип командам',toho)
+						continue
+					if user_mode == 0:
+						apisay('Вам бан',toho)
+						continue
+					if user_mode > 0:
+						threading.Thread(target=do_cmd,args=(commands['default'][cmds[text_split[1]][1]],pack)).start()		
+						continue			
+				else:
+					speak = requests.post('https://isinkin-bot-api.herokuapp.com/1/talk',data={'q':user_text}).json()
+					if 'text' in speak: apisay(speak['text'],toho)
+					else: apisay('Команда не найдена :(', toho)
+	except:
+		except Exception as error:
+			if error == KeyboardInterrupt:
+							sys.exit(0)
+			print(error)
+			continue
