e964209f0c2983c619db3d9ce7a6332ec3e4ea8b
Cha14ka <taitor@yandex.ru>
2019-06-30 02:33:40 +0300

    быстрофикс

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ token = config['group_token']
 def apisay(text,toho,attachment=None,keyboard={"buttons":[],"one_time":True}):
 	requests.post('https://api.vk.com/method/messages.send',data={'access_token':token,'v':'5.80','peer_id':toho,'message':text,'attachment':attachment,'keyboard':json.dumps(keyboard,ensure_ascii=False)}).json()
 def sendpic(pic,mess,toho,keyboard={"buttons":[],"one_time":True}):
-	if True:
+	try:
 		ret = requests.get('https://api.vk.com/method/photos.getMessagesUploadServer?access_token={access_token}&v=5.68'.format(access_token=token)).json()
 		try:
 			with open(pic, 'rb') as f:
@@ -22,8 +22,8 @@ def sendpic(pic,mess,toho,keyboard={"buttons":[],"one_time":True}):
 			ret = requests.post(ret['response']['upload_url'],files={'photo':('photo.png',pic,'image/png')}).json()
 		ret = requests.get('https://api.vk.com/method/photos.saveMessagesPhoto?v=5.68&album_id=-3&server='+str(ret['server'])+'&photo='+ret['photo']+'&hash='+str(ret['hash'])+'&access_token='+token).json()
 		requests.post('https://api.vk.com/method/messages.send',data={'attachment':'photo'+str(ret['response'][0]['owner_id'])+'_'+str(ret['response'][0]['id']),'message':mess,'v':'5.80','peer_id':str(toho),'access_token':str(token),'keyboard':json.dumps(keyboard)})
-	'''except KeyError:
-		apisay('Что-то пошло не так :(',toho) '''
+	except KeyError:
+		apisay('Что-то пошло не так :(',toho)
 def do_cmd(code,pack):
 	exec(code)
 			
